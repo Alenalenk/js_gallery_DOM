fc152d36b881c4872403b470d88221e0746ab779
Olena <113938017+Alenalenk@users.noreply.github.com>
2024-06-17 12:18:07 +0300

    make build

---
+++ index.6083819c.js
@@ -0,0 +1,2 @@
+var e=document.querySelector("img#largeImg");document.querySelector("ul#thumbs").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest("a").href;e.src=r});
+//# sourceMappingURL=index.6083819c.js.map
